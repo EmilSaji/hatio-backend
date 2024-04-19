5b99bf93065ad327ac9562b377a8031b5b61ec00
Emil Saji <emilsaji123@gmail.com>
2024-04-19 13:18:52 +0530

    Feat: Server and Databse Setup

---
+++ server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require('cors');
+const mysql = require("mysql2/promise");
+
+const app = express();
+app.use(cors()); 
+app.use(express.json());
+
+mysql
+  .createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: true,
+      ...(process.env.CA_CERT_PATH && {
+        ca: require("fs").readFileSync(process.env.CA_CERT_PATH),
+      }),
+    },
+  })
+  .then((connection) => {
+    console.log("Successfully connected to MySQL database!");
+    app.locals.connection = connection;
+
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send("Something broke!");
+    });
+
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Failed to connect to the database: ${error.message}`);
+    process.exit(1);
+  });
